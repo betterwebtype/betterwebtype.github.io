fe8ae3b340f303ed794827c0e4b1ca54af428e39
Matej Latin <matejlatin@gmail.com>
2017-02-14 16:16:18 +0000

    Updated js files

--- assets/combined.js
+++ assets/combined.js
@@ -1,39 +1,6 @@
 (function(window, document) {
   'use strict';
 
-  // set the date we're counting down to
-  var target_date = new Date("Feb 14, 2017").getTime();
-
-  // variables for time units
-  var days, hours, minutes, seconds;
-
-  // get tag element
-  var countdown = document.getElementById("countdown");
-
-  // update the tag with id "countdown" every 1 second
-  setInterval(function () {
-
-    // find the amount of "seconds" between now and target
-    var current_date = new Date().getTime();
-    var seconds_left = (target_date - current_date) / 1000;
-
-    // do some time calculations
-    days = parseInt(seconds_left / 86400);
-    seconds_left = seconds_left % 86400;
-
-    hours = parseInt(seconds_left / 3600);
-    seconds_left = seconds_left % 3600;
-
-    minutes = parseInt(seconds_left / 60);
-    seconds = parseInt(seconds_left % 60);
-
-    // format countdown string + set tag value
-    // countdown.innerHTML = days + "days " + hours + "hours "
-    // + minutes + "m " + seconds + "s ";
-    countdown.innerHTML = "<div class='days'>" + days + "<br><span>days</span></div><div class='hours'>" + hours + "<br><span>hours</span></div><div class='minutes'>" + minutes + "<br><span>minutes</span></div><div class='seconds'>" + seconds + "<br><span>seconds</span></div>";
-
-  }, 1000);
-
   var flip = 0;
 
   function toggleGrid() {
