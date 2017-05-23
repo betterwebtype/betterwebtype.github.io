e079372c277da7e6e96c876e85b4f350137ce006
Matej Latin <matejlatin@gmail.com>
2017-05-23 15:53:10 +0100

    Fixing the BWT form

--- src/js/main.js
+++ src/js/main.js
@@ -33,7 +33,7 @@
     var $this = $(this);
     $.ajax({
       type: "GET",
-      url: 'http://betterwebtype.us2.list-manage2.com/subscribe/post-json?c=?',
+      url: 'https://betterwebtype.us2.list-manage2.com/subscribe/post-json?c=?',
       data: $this.serialize(),
       dataType: 'json',
       contentType: "application/json; charset=utf-8",
